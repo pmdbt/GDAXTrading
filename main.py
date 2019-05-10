73e0d82962623c164f3fc36162172bd163dc7c98
JerryChu <jerrychu123@gmail.com>
2019-05-09 18:18:27 -0700

    syntax fix

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def main():
 
         # organize data for uploading to mysql
         today_close_price, prediction = trading_obj.execute()
-        if past_data.empty() == False:
+        if past_data.empty == False:
             upload_data = organize_upload(
                 today_close_price,
                 prediction,
